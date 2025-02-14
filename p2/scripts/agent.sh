41bac203d86f1520e259313a1597910ec049a8f6
Tristan CHANTROUX <tchantro@paul-f4Ar4s2.clusters.42paris.fr>
2025-02-14 17:32:13 +0100

    config ok

--- p2/scripts/agent.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -sfL https://get.k3s.io | sh -s - agent --server https://192.168.56.110:6443 --token-file /vagrant/token/node-token --flannel-iface=eth1 --node-ip=192.168.56.111
--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -4,3 +4,11 @@ curl -sfL https://get.k3s.io | sh -s - server --flannel-iface=eth1 --node-ip=192
 
 mkdir -p /vagrant/token
 cp /var/lib/rancher/k3s/server/node-token /vagrant/token
+
+sudo kubectl create -f /vagrant/apps/app1.yaml
+sudo kubectl create -f /vagrant/apps/app2.yaml
+sudo kubectl create -f /vagrant/apps/app3.yaml
+sudo kubectl create -f /vagrant/service/app1.yaml
+sudo kubectl create -f /vagrant/service/app2.yaml
+sudo kubectl create -f /vagrant/service/app3.yaml
+sudo kubectl apply -f /vagrant/ingress.yaml
