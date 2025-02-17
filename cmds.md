4eda7bfb616d0b4d822c36b5226e1b0d5022483d
cloosafm <alexandra.cloos@gmail.com>
2025-02-17 18:52:44 +0100

    yaml written abd checked for p2, but not working (error 404)

---
+++ cmds.md
@@ -0,0 +1,13 @@
+k get pods
+k get svc
+k get ingress
+
+
+
+curl -H "Host:app1.com" 192.168.56.110
+
+
+to view the nginx logs:
+k get pods
+-> use output pod name as
+k logs <pod-name>
---
+++ p2/scripts/server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "alias k='sudo kubectl'" >> /home/vagrant/.bashrc
+source ~/.bashrc
+
+sudo apt-get update
+sudo apt-get upgrade
+
+curl -sfL https://get.k3s.io | sh -s - server --flannel-iface=eth1 --node-ip=$SERVER_IP
+
+mkdir -p /vagrant/token
+cp /var/lib/rancher/k3s/server/node-token /vagrant/token
+
+echo -e "192.168.56.110 app1.com" | sudo tee -a /etc/hosts
+echo -e "192.168.56.110 app2.com" | sudo tee -a /etc/hosts
+echo -e "192.168.56.110 app3.com" | sudo tee -a /etc/hosts
+
+sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+
+
+sudo kubectl apply -f /vagrant/confs/deployment.yaml
+sudo kubectl apply -f /vagrant/confs/service.yaml
+sudo kubectl apply -f /vagrant/confs/ingress.yaml
+
