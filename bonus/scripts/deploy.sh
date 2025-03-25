35b1305264999bb0aa838666a5e76d5d06fbc1a7
Alexandra CLOOS <alexandra.cloos@gmail.com>
2025-03-25 11:35:35 +0100

    fixed typo in bonus script

--- bonus/scripts/deploy.sh
+++ bonus/scripts/deploy.sh
@@ -9,8 +9,8 @@ echo "Deployment of YAML files..."
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 # Appliquer les fichiers YAML
-kubectl apply -f "$BASE_DIR/conf/deployment.yaml"
-kubectl apply -f "$BASE_DIR/conf/service.yaml"
-kubectl apply -f "$BASE_DIR/conf/argo-application.yaml"
+kubectl apply -f "$BASE_DIR/confs/deployment.yaml"
+kubectl apply -f "$BASE_DIR/confs/service.yaml"
+kubectl apply -f "$BASE_DIR/confs/argo-application.yaml"
 
 echo "Everything is setup! Success!"
